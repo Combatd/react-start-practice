2428f67b98c7ddbd14480ed9e0dd9d59305c54e3
Combatd <combatdstar@gmail.com>
2020-09-02 14:41:19 -0700

    useState for count in App.js

--- sea_react/src/App.js
+++ sea_react/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCounter] = useState(0);
+  console.log(count);
   return (
+    // JSX
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>COUNTER</div>
+      <div>{count}</div>
+      <button onClick={ () => setCounter(count + 1)}>+</button>
+      <button>-</button>
     </div>
   );
 }
