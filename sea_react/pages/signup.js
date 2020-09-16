f8175413ec0da631ad0a663e4b14e543d8588b25
Combatd <combatdstar@gmail.com>
2020-09-16 14:57:33 -0700

    Define doSubmitForm event handler

--- sea_react/pages/signup.js
+++ sea_react/pages/signup.js
@@ -1,6 +1,17 @@
 function Signup() {
+    const doSubmitForm = (e) => {
+        e.preventDefault();
+    }
+
     return(
-        <div>this is the signup page</div>
+        <div>
+            <h1>this is the signup page</h1>
+            <form onSubmit={doSubmitForm}>
+                <input name='email' type='text' />
+                <input name='password' type='password' />
+                <button type='submit'>SUBMIT</button>
+            </form>
+        </div>
     )
 }
 
