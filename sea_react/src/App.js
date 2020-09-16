a4dccf9afa620fe4ffdb84faa7de70b39ef66e22
Combatd <combatdstar@gmail.com>
2020-09-16 14:28:29 -0700

    Clear out Parent App.js"

--- sea_react/src/App.js
+++ sea_react/src/App.js
@@ -6,23 +6,12 @@ import Button from './components/Button';
 
 function App() {
   // Whatever you pass useState() is the initial State
-  const [count, setCounter] = useState(0);
-
-  const addToCount = () => {
-    setCounter(count + 1);
-  }
-
-  const subtractFromCount = () => {
-    setCounter(count - 1);
-  }
+ 
 
   return (
     // JSX
     <div className="App">
-      <div>COUNTER</div>
-      <div className={ (count < 0) ? 'red' : ''}>{count}</div>
-      <Button color='pink' onBtnClick={addToCount}>+</Button>
-      <Button color='green' onBtnClick={subtractFromCount}>-</Button>
+      This is the landing page!
     </div>
   );
 }
