ef576ace5047a18bdb1d1340fa7e7f7a4a7f603c
Combatd <combatdstar@gmail.com>
2020-09-16 15:05:20 -0700

    Set state with hooks in Signup

--- sea_react/pages/signup.js
+++ sea_react/pages/signup.js
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const doSubmitForm = (e) => {
         e.preventDefault();
+        console.log(email, password)
     }
+
 
     return(
         <div>
             <h1>this is the signup page</h1>
             <form onSubmit={doSubmitForm}>
-                <input name='email' type='text' />
-                <input name='password' type='password' />
-                <button type='submit'>SUBMIT</button>
+                <input name='email' type='text' onChange={(e) => setEmail(e.target.value)} />
+                <input 
+                    name='password' 
+                    type='password' 
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button type='submit' >SUBMIT</button>
             </form>
         </div>
     )
